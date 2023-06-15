aa2074ab10051e4777e13b432ce37eeb4cf9e34a
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-06-15 10:04:37 +0300

    feat: add store creation & Zustand

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,6 +1,21 @@
-const useTaskManager = () => {
-
-}
+import create from 'zustand';
+// Définition du store Zustand pour gérer les tâches
+const useTaskManager = create((set) => ({
+  tasks: [],
+  searchKeyword: '',
+  setSearchKeyword: (keyword:string) => set(() => ({ searchKeyword: keyword })),
+  addTask: (task :{ id: number; title: string }) =>  set((state: { tasks: { id: number; title: string }[] }) => ({ tasks: [...state.tasks, task] })),
+  updateTask: (taskId: number, updatedTask : {title:string}) =>
+    set((state: {tasks:{ id: number; title: string }[]}) => ({
+      tasks: state.tasks.map((task) =>
+        task.id === taskId ? { ...task, ...updatedTask } : task
+      ),
+    })),
+  deleteTask: (taskId: number) =>
+  set((state: {tasks:{ id: number; title: string }[]}) => ({
+      tasks: state.tasks.filter((task) => task.id !== taskId),
+    })),
+}));
 
 export {
   useTaskManager
