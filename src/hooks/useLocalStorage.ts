ad91f39559bef8aa86763a7d110e19a6490db297
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-06-15 11:25:47 +0300

    feat: add creating hook with local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,16 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from 'react';
+const useLocalStorage = <T>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
 
-}
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
 
 export {
   useLocalStorage
